86313a2aa1dc7951a2cdc303e91a02668b1956ec
Botaaaa <108619302+Botaaaa@users.noreply.github.com>
2022-07-06 11:35:43 +0530

    config.js

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.owner = ['94740477644'] //ur owner number
+global.ownername = "Dark Devil OFC ©"
+global.ytname = "YT: Xeon" //11K Subs
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
+global.botname = "DARK DEVIL OFC" //ur bot name
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
 global.author = "🦄Dream Guy Xeon"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.themeemoji = "🖤"
+global.reactmoji = "🖤"
+global.ownertag = ['94740477644'] //ur owner tag
+global.ownernummenu = ['94740477644'] //ur owner number in menu and all
+global.watermark = "CheemsBot MD v3." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
 global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
@@ -102,8 +102,8 @@ global.mess = {
     private: 'This feature is only for private chats!',
     bot: 'This feature is only for bot',
     wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!',
+    linkm: 'Link එක දාපන් පගෝ?',
+    error: 'අඩෝ Error බන්ග්!',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
     banChat: 'The bot was banned in this group, please contact the owner to unban'
